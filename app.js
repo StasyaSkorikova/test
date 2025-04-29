8ebe0bdbceb920ff75d4d262498959540683877d
StasyaSkorikova <anastasiaskorikova98@gmail.com>
2025-04-29 14:26:59 +0300

    Добавила переменные обо мне

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 let firstName = 'Stasya'
 firstName = 'Anastasiia'
 console.log('Hello', firstName)
+const name = "Stasya";
+const age = 27;
+const city = "Москва";
+const hobby = "программирование";
+
+console.log("Меня зовут " + name);
+console.log("Мне " + age + " лет");
+console.log("Я живу в городе " + city);
+console.log("Моё хобби — " + hobby);
